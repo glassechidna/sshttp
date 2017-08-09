27fd13d80f5af2696ed103a7772eee1b53a69327
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-08-10 09:25:10 +1000

    Initial commit

---
+++ src/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euxo pipefail
+mkdir -p ~/.ssh
+echo "localhost $SSH_KNOWN_HOST" >> ~/.ssh/known_hosts
+exec ssh -o ProxyCommand="websocat -q - ws://${ENDPOINT}" -R ${REMOTE_SSH_FWD_PORT}:${DOCKER_HOST_ADDR}:22 ${SSH_USER}@localhost
