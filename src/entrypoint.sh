73f36cc00b2cfd5ae41e80adf474612a89b3bcbb
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-08-10 10:23:57 +1000

    No need for TTY or remote command

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 mkdir -p ~/.ssh
 echo "localhost $SSH_KNOWN_HOST" >> ~/.ssh/known_hosts
-exec ssh -o ProxyCommand="websocat -q - ws://${ENDPOINT}" -R ${REMOTE_SSH_FWD_PORT}:${DOCKER_HOST_ADDR}:22 ${SSH_USER}@localhost
+exec ssh -o ProxyCommand="websocat -q - ws://${ENDPOINT}" -R ${REMOTE_SSH_FWD_PORT}:${DOCKER_HOST_ADDR}:22 -N -T ${SSH_USER}@localhost
